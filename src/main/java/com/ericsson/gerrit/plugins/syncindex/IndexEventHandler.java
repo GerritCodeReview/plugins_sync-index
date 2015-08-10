08675a8df3f3b710c9a140f6aff2d6a4ea9a030a
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2015-08-10 17:00:43 -0400

    sync-index plugin - Initial version
    Sync-index plugin keeps the secondary index synchronized among two
    Gerrit instances that share the same git repositories and Gerrit
    database.
    
    Using a new extension point in Gerrit core [1], this plugin is notified
    every time a change has been indexed in or removed from the secondary
    index. Then it propagates this information to the configured target
    Gerrit instance. The sync-index plugin installed in the target instance
    updates the corresponding secondary index accordingly.
    
    [1] https://gerrit-review.googlesource.com/#/c/72607/
    
    Change-Id: Id5c089cfe3263f23dd56880e47d609180aeb3edc

---
+++ src/main/java/com/ericsson/gerrit/plugins/syncindex/IndexEventHandler.java
@@ -0,0 +1,77 @@
+// Copyright (C) 2015 Ericsson
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.syncindex;
+
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.events.ChangeIndexedListener;
+import com.google.inject.Inject;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+class IndexEventHandler implements ChangeIndexedListener {
+  private final ScheduledThreadPoolExecutor executor;
+  private final RestSession restClient;
+  private final String pluginName;
+
+  @Inject
+  IndexEventHandler(@SyncIndexExecutor ScheduledThreadPoolExecutor executor,
+      @PluginName String pluginName,
+      RestSession restClient) {
+    this.restClient = restClient;
+    this.executor = executor;
+    this.pluginName = pluginName;
+  }
+
+  @Override
+  public void onChangeIndexed(Event event) {
+    executeIndexTask(event, false);
+  }
+
+  @Override
+  public void onChangeDeleted(Event event) {
+    executeIndexTask(event, true);
+  }
+
+  private void executeIndexTask(Event event, boolean removed) {
+    if (!Context.isForwardedEvent()) {
+      executor.execute(new SyncIndexTask(event, removed));
+    }
+  }
+
+  class SyncIndexTask implements Runnable {
+    private ChangeIndexedListener.Event event;
+    private boolean removed;
+
+    SyncIndexTask(Event event, boolean removed) {
+      this.event = event;
+      this.removed = removed;
+    }
+
+    @Override
+    public void run() {
+      if (removed) {
+        restClient.deleteFromIndex(event);
+      } else {
+        restClient.index(event);
+      }
+    }
+
+    @Override
+    public String toString() {
+      return String.format("[%s] Index change %s in target instance",
+          pluginName, event.getChangeId());
+    }
+  }
+}
---
+++ src/test/java/com/ericsson/gerrit/plugins/syncindex/IndexEventHandlerTest.java
@@ -0,0 +1,90 @@
+// Copyright (C) 2015 Ericsson
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.ericsson.gerrit.plugins.syncindex;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.easymock.EasyMock.expect;
+
+import com.google.gerrit.extensions.events.ChangeIndexedListener.Event;
+
+import org.easymock.EasyMockSupport;
+import org.junit.Test;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class IndexEventHandlerTest extends EasyMockSupport {
+  private static final String PLUGIN_NAME = "sync-index";
+
+  private Event eventMock;
+  private IndexEventHandler indexEventHandler;
+  private ScheduledThreadPoolExecutor pool;
+  private RestSession restClient;
+
+  @Test
+  public void testChangeIndexedEventHandler() throws Exception {
+    setUpMocks(true);
+    resetAll();
+    expect(restClient.index(eventMock)).andReturn(true);
+    replayAll();
+    indexEventHandler.onChangeIndexed(eventMock);
+    verifyAll();
+  }
+
+  @Test
+  public void testChangeDeletedEventHandler() throws Exception {
+    setUpMocks(true);
+    resetAll();
+    expect(restClient.deleteFromIndex(eventMock)).andReturn(true);
+    replayAll();
+    indexEventHandler.onChangeDeleted(eventMock);
+    verifyAll();
+  }
+
+  @Test
+  public void testIndexEventHandlerIsForwarded() throws Exception {
+    setUpMocks(false);
+    Context.setForwardedEvent(true);
+    indexEventHandler.onChangeIndexed(eventMock);
+    indexEventHandler.onChangeDeleted(eventMock);
+    Context.unsetForwardedEvent();
+    verifyAll();
+  }
+
+  private void setUpMocks(boolean mockRestClient) {
+    eventMock = createNiceMock(Event.class);
+    pool = new PoolMock(1);
+    if (mockRestClient) {
+      restClient = createMock(RestSession.class);
+    } else {
+      restClient = null;
+    }
+    replayAll();
+    indexEventHandler = new IndexEventHandler(pool, PLUGIN_NAME, restClient);
+  }
+
+  private class PoolMock extends ScheduledThreadPoolExecutor {
+    PoolMock(int corePoolSize) {
+      super(corePoolSize);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+      assertThat(command.toString()).isEqualTo(
+          String.format("[%s] Index change %s in target instance",
+              PLUGIN_NAME, 0));
+      command.run();
+    }
+  }
+}
